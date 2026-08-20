ec68e15713dfff606bbb477cf6832d3fa1d93643
Commercial <135322806+alinavasileva-cco@users.noreply.github.com>
2026-08-20 19:18:23 +0300

    Move clients into hero and remove standalone client table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Clients from './components/Clients';
 import Services from './components/Services';
 import Profile from './components/Profile';
 import Expertise from './components/Expertise';
@@ -42,7 +41,6 @@ export default function App() {
       <Header />
       <main className="site-main" id="main-content">
         <Hero />
-        <Clients />
         <Services />
         <Profile />
         <Expertise />
