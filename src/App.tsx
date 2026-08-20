c36f134eedbfcfb02206f5aa281ca0d41ae413e2
Commercial <135322806+alinavasileva-cco@users.noreply.github.com>
2026-08-20 20:25:23 +0300

    Remove duplicate expertise section and simplify site flow

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Profile from './components/Profile';
-import Expertise from './components/Expertise';
 import Cases from './components/Cases';
 import Experience from './components/Experience';
 import Approach from './components/Approach';
@@ -43,7 +42,6 @@ export default function App() {
         <Hero />
         <Services />
         <Profile />
-        <Expertise />
         <Cases />
         <Experience />
         <Approach />
