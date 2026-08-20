4252be77cfde4bda62604bb52a668ead59df799d
Commercial <135322806+alinavasileva-cco@users.noreply.github.com>
2026-08-20 22:49:51 +0500

    Use one final V9 stylesheet instead of conflicting legacy layout layers

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/tokens.css';
 import './styles/global.css';
-import './styles/layout.css';
-import './styles/responsive.css';
 import './styles/integrated.css';
 
 document.documentElement.classList.add('js');
